276c812549e647f082874d1fdad757d51ae719fa
urvi-28 <90060335+urvi-28@users.noreply.github.com>
2022-06-27 23:27:36 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_MnkBwhVLZ', // e.g. us-east-2_uXboG5pAb
-        clientId: '3ahjei0mrigsrdi14k4to9u0pj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: '', // e.g. us-east-2_uXboG5pAb
+        clientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-2
     },
-    // api: {
-    //     invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    // }
 };
