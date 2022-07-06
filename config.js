8b9ab933aafee0f7374acc6f89624ff0a656cde0
urvi-28 <90060335+urvi-28@users.noreply.github.com>
2022-07-06 21:53:07 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: '', // add your user pool id
+        clientId: '', // add you client id
+        region: '' // e.g. us-east-2
+    },
+   
+};
