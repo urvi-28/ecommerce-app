0e000c69f004e2822dca73a0cb0e305f63a9ec80
urvi-28 <90060335+urvi-28@users.noreply.github.com>
2022-07-06 22:21:43 +0530

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-window._config = {
-    cognito: {
-        userPoolId: '', // add your user pool id
-        clientId: '', // add you client id
-        region: '' // e.g. us-east-2
-    },
-   
-};
